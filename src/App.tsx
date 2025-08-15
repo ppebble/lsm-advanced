d68efaef5680c9d18424531ae012bba027f706da
leesm <ask135799@naver.com>
2025-08-16 02:20:57 +0900

    테스트용 Banner 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import Banner from './components/Banner';
 
 function App() {
-	const url = import.meta.env.VITE_TEST_URL1;
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> a nd save to reload. {url}
-					한국어 한국어
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<div></div>
+			<Banner />
+		</>
 	);
 }
 
---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,50 @@
+import { css } from 'styled-system/css';
+
+function Banner() {
+	return (
+		<div className={css({ fontFamily: 'sans', minHeight: '100vh', bg: 'gray.50' })}>
+			<section
+				className={css({
+					height: '500px',
+					bgImage: 'url(/asset/img/id01.jpg)',
+					bgSize: 'cover',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					color: 'white',
+					textAlign: 'center',
+				})}
+			>
+				<div>
+					<h1
+						className={css({
+							fontSize: { base: '3xl', md: '5xl' },
+							fontWeight: 'bold',
+							textShadow: '0 2px 4px rgba(0,0,0,0.5)',
+							color: 'gray.200',
+						})}
+					>
+						당신의 공간을 변화시키세요
+					</h1>
+					<p className={css({ fontSize: 'xl', mt: '4' })}>
+						300+ 전문 업체와 연결되는 인테리어 플랫폼
+					</p>
+					<button
+						className={css({
+							mt: '6',
+							px: '6',
+							py: '3',
+							bg: 'orange.500',
+							color: 'white',
+							rounded: 'md',
+							_hover: { bg: 'orange.600' },
+						})}
+					>
+						무료 상담 신청
+					</button>
+				</div>
+			</section>
+		</div>
+	);
+}
+export default Banner;
