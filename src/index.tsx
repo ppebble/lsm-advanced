a0790475445bc24837b3d25b38252e8cff7b45b9
leesm <ask135799@naver.com>
2025-08-30 03:01:45 +0900

    개발환경 msw worker 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/fonts.css';
+import { worker } from '../mocks/browser';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+if (process.env.NODE_ENV === 'development') {
+	worker.start({
+		onUnhandledRequest: 'bypass', // MSW가 처리하지 않은 요청은 그대로 통과
+	});
+}
 root.render(
 	<React.StrictMode>
 		<App />
