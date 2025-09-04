024996907bd95d80e0d5646017e8cb8ea3d01732
leesm <ask135799@naver.com>
2025-09-04 18:13:56 +0900

    트렌드 컴포넌트 fetch 방식으로 수정, 이미지 최적화 추가

--- src/components/Trend/index.tsx
+++ src/components/Trend/index.tsx
@@ -3,18 +3,47 @@ import { flex } from 'styled-system/patterns';
 import { Link, useNavigate } from 'react-router-dom';
 import { trendPatterns, trendStyles } from './styles';
 import { trends } from '@/assets/data/psudoData';
+import { useIntersectionObserver } from '@/hooks/useIntersectionObserver';
+import { useEffect, useState } from 'react';
+import { ApiResponse, TrendProps } from '@/assets/data/type';
 
 function Trend() {
-	const navigate = useNavigate();
+	// const { ref } = useIntersectionObserver();
+	const refCallback = useIntersectionObserver();
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<String | null>(null);
+	const [trendItems, setTrendItems] = useState<TrendProps[]>([]);
+	useEffect(() => {
+		const fetchTrends = async () => {
+			try {
+				const res = await fetch('/api/trends');
+				if (!res.ok) {
+					throw new Error('실시간 트렌드 조회에 실패했습니다');
+				}
+				const result: ApiResponse<TrendProps[]> = await res.json();
+				if (result) {
+					setTrendItems(result.data);
+				}
+			} catch (err: any) {
+				setError(err.messsage);
+			}
+		};
+		fetchTrends();
+	}, []);
 
 	return (
 		<>
 			<h2 className={trendStyles.title}>🏆 실시간 인기 시공 사례</h2>
 
 			<div className={trendStyles.gridContainer}>
-				{trends.map((item) => (
+				{trendItems.map((item) => (
 					<div key={item.id} className={trendStyles.card}>
-						<img src={item.img} className={trendStyles.image} />
+						<img
+							ref={refCallback}
+							data-src={item.img}
+							className={trendStyles.image}
+							alt='Loading . . .'
+						/>
 						<div className={css({ p: '4' })}>
 							<h3 className={css({ fontSize: 'lg', fontWeight: 'bold', mb: '2', lineClamp: 2 })}>
 								{item.title}
@@ -28,12 +57,9 @@ function Trend() {
 								<span className={css({ color: 'amber.600', fontWeight: 'bold' })}>
 									♥ {item.likes}
 								</span>
-								<button
-									className={trendStyles.detailButton}
-									onClick={() => navigate(`/trend/${item.id}`)}
-								>
+								<Link className={trendStyles.detailButton} to={`/trend/${item.id}`}>
 									상세보기
-								</button>
+								</Link>
 							</div>
 						</div>
 					</div>
@@ -41,9 +67,9 @@ function Trend() {
 			</div>
 
 			<div className={trendPatterns.flexCenter}>
-				<button className={trendStyles.moreButton} onClick={() => navigate('/trends')}>
+				<Link className={trendStyles.moreButton} to={'/trends'}>
 					더 많은 사례 보기 →
-				</button>
+				</Link>
 			</div>
 		</>
 	);
