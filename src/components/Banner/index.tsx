48e06f1c36eac7bd656022eb0b96b7ae34d867b8
leesm <ask135799@naver.com>
2025-09-07 04:27:26 +0900

    배너 컴포넌트 슬라이드로 변경 및 슬라이드 컴포넌트 추가

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,41 +1,13 @@
+import { useRef, useState } from 'react';
 import { css } from 'styled-system/css';
 import { bannerStyles } from './styles';
-import { useEffect, useState } from 'react';
-import { ApiResponse, BannerItems } from '@/assets/data/type';
-function Banner() {
-	const [loading, setLoading] = useState<boolean>(true);
-	const [error, setError] = useState<String | null>(null);
-	const [bannerItems, setBannerItems] = useState<BannerItems[]>([]);
-	useEffect(() => {
-		const fetchBanners = async () => {
-			try {
-				const res = await fetch('/api/banners');
-				if (!res.ok) {
-					throw new Error('배너 이미지 조회에 실패했습니다');
-				}
-				const result: ApiResponse<BannerItems[]> = await res.json();
-				if (result) {
-					setBannerItems(result.data);
-				}
-			} catch (err: any) {
-				setError(err.messsage);
-			}
-		};
-		fetchBanners();
-	}, []);
+import { BannerItems } from '@/assets/data/type';
+import { useFetch } from '@/hooks/useFetch';
+import Slider from './Slider';
 
-	return (
-		<>
-			<div className={bannerStyles.mainImage}>
-				<div>
-					<h1 className={bannerStyles.mainPhrase}>당신의 공간을 변화시키세요</h1>
-					<p className={css({ fontSize: 'xl', mt: '4' })}>
-						300+ 전문 업체와 연결되는 인테리어 플랫폼
-					</p>
-					<button className={bannerStyles.btnInquiry}>무료 상담 신청</button>
-				</div>
-			</div>
-		</>
-	);
+function Banner() {
+	const { data: bannerItems, loading, error } = useFetch<BannerItems[]>('/api/banners');
+
+	return <>{bannerItems && <Slider bannerItems={bannerItems} />}</>;
 }
 export default Banner;
