c985288bdc0f0911ba9e073fa99b0e4b0c8e1060
leesm <ask135799@naver.com>
2025-09-09 19:54:01 +0900

    배너 컴포넌트 api url 변경(문자열 > 상수)

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,11 @@
 import type { BannerItems } from '@/assets/data/type';
 import { useFetch } from '@/hooks/useFetch';
+import { SERVICE_URLS } from '@/utils/ServiceUrls';
 
 import Slider from './Slider';
 
 const Banner = () => {
-	const bannerItems = useFetch<BannerItems[]>({ url: '/api/banners' });
+	const bannerItems = useFetch<BannerItems[]>({ url: SERVICE_URLS.banners });
 
 	return <div>{bannerItems.data && <Slider bannerItems={bannerItems.data} />}</div>;
 };
