358de8362047d1d122ad8fe238b7fba5bb8046ab
leesm <ask135799@naver.com>
2025-09-12 02:08:45 +0900

    useIntersectionObserver 훅 정리(미사용 import 제거)

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,11 +1,10 @@
-import { useEffect, useRef, useCallback } from 'react';
+import { useRef, useCallback } from 'react';
 
 /**
  * lazy loading을 위한  IntersectionOberserver 훅
  * usage :: 사용해야 하는 img 태그에 ref={refCallback} 과 data-src 속성 추가
  * @returns ref 콜백 함수
  */
-
 export const useIntersectionObserver = () => {
 	const observerRef = useRef<IntersectionObserver | null>(null);
 
