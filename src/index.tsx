74fb7c0fa424f4bccdb0dbd8b6a29dd0066641bc
leesm <ask135799@naver.com>
2025-09-16 20:42:00 +0900

    QueryProvider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,6 +6,17 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import './styles/fonts.css';
+
+// QueryClient 생성
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			throwOnError: true,
+
+			retry: false, // 선택: 개발 중엔 자동 재시도 끄면 디버깅 편해요
+		},
+	},
+});
 
 async function enableMocking() {
 	if (process.env.NODE_ENV !== 'development') {
@@ -14,13 +26,16 @@ async function enableMocking() {
 	const { worker } = await import('../mocks/browser');
 	return worker.start();
 }
+
 enableMocking().then(() => {
 	const rootElement = document.getElementById('root');
 	if (rootElement) {
 		ReactDOM.createRoot(rootElement).render(
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>,
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</QueryClientProvider>,
 		);
 	} else {
 		console.error('Root element not found');
