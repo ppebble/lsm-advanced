99c6235133f2ebc8ef879ec6681cf445bddfcb9c
leesm <ask135799@naver.com>
2025-09-16 21:53:50 +0900

    fetcher 수정

--- src/utils/FeachResult.ts
+++ src/utils/FeachResult.ts
@@ -1,8 +1,3 @@
-type FetchResult<T> = {
-	data: T;
-	// 필요하면 다른 속성 추가 가능 (예: status, refetch)
-};
-
 export async function fetcher<T>(url: string, options?: RequestInit): Promise<T> {
 	const res = await fetch(url, options);
 	if (!res.ok) throw new Error(`Fetch 실패: ${res.status}`);
