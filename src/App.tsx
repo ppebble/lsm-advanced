2577923adbf43d3c2359b9002fa949848ef096b4
leesm <ask135799@naver.com>
2025-09-27 23:53:35 +0900

    업체 탐색 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Navigation from './components/Navigation';
+import CompanyExplorer from './pages/companyExplore';
 import DetailItem from './pages/detailItem';
 import Home from './pages/Home';
 
@@ -10,6 +11,7 @@ const App = () => {
 			<Route element={<Navigation />}>
 				<Route path='/' element={<Home />} />
 				<Route path='/work/:id' element={<DetailItem />} />
+				<Route path='/companies' element={<CompanyExplorer />} />
 			</Route>
 		</Routes>
 	);
